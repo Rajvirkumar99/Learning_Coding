549775246c71db3fb3f194629078b21430a169fa
RAJVIR KUMAR <84075116+Rajvirkumar99@users.noreply.github.com>
2022-10-29 14:31:43 +0530

    Create Find_age_group.cpp

---
+++ Find_age_group.cpp
@@ -0,0 +1,13 @@
+int main() {
+    int age ;
+    cin>>age;
+    if (age <=12){
+        cout<<"Child";
+    }
+    else if (age >12 && age <=18 ){
+        cout<<"Teenager";
+    }
+    else {
+        cout<<"Adult";
+    }
+}
